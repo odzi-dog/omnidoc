b165065746c684ebbe24e50cd1831dae8c8706a3
julipup <dingolediego@gmail.com>
2023-03-16 22:23:07 +0200

    Add data directory, test custom express server and change sqlite file locations

--- src/lib/auth/database.ts
+++ src/lib/auth/database.ts
@@ -1,4 +1,3 @@
 import SqliteProvider from "better-sqlite3";
-import { dev as isDev } from "$app/environment";
 
-export const Database = new SqliteProvider(`${ isDev ? "" : "/data/" }auth.sqlite3`);
+export const Database = new SqliteProvider(`./data/auth.sqlite3`);
--- src/lib/database/DatabaseInstance.ts
+++ src/lib/database/DatabaseInstance.ts
@@ -1,10 +1,9 @@
 import { MikroORM } from '@mikro-orm/core';
 import { TextDocument, Workspace } from './entities';
-import { dev as isDev } from '$app/environment';
 
 const instance = await MikroORM.init({
     entities: [TextDocument, Workspace],
-    dbName: `${ isDev ? "" : "/data/" }data.sqlite3`,
+    dbName: `./data/data.sqlite3`,
     type: 'better-sqlite',
     migrations: {
         path: "migrations",
