9cd760c160231cdc149a0e127c071495d4de717b
julipup <dingolediego@gmail.com>
2023-04-03 18:23:09 +0300

    Trying to fix centrifugo's getClient function

--- src/lib/synchronization/client/getClient.ts
+++ src/lib/synchronization/client/getClient.ts
@@ -2,7 +2,7 @@ import { env } from "$env/dynamic/public";
 import { Centrifuge } from "centrifuge";
 
 export function getClient(autoConnect = false) {
-    const client = new Centrifuge(`ws://${ env.PUBLIC_CENTRIFUGO_URL }/connection/websocket`, {});
+    const client = new Centrifuge(`${ env.PUBLIC_CENTRIFUGO_URL }/connection/websocket`, {});
 
     if (autoConnect) client.connect();
 
