e8280e787441c85d32896f4669c96da2c13a5c76
julipup <dingolediego@gmail.com>
2023-04-06 18:28:28 +0300

    Trying to fix build pipeline

--- src/lib/database/SynchronizationInstance.ts
+++ src/lib/database/SynchronizationInstance.ts
@@ -2,7 +2,7 @@ import { env } from "$env/dynamic/public";
 import { Centrifuge } from "centrifuge";
 import ServerWebSocket from "ws";
 
-const ServerSynchronizationClient = new Centrifuge(`${ env.PUBLIC_CENTRIFUGO_URL }/connection/websocket`, {
+const ServerSynchronizationClient = new Centrifuge(`${ env.PUBLIC_CENTRIFUGO_URL ?? "ws://localhost:8001" }/connection/websocket`, {
     websocket: ServerWebSocket,
 });
 ServerSynchronizationClient.connect();
--- src/lib/synchronization/client/getClient.ts
+++ src/lib/synchronization/client/getClient.ts
@@ -2,7 +2,7 @@ import { env } from "$env/dynamic/public";
 import { Centrifuge } from "centrifuge";
 
 export function getClient(autoConnect = false) {
-    const client = new Centrifuge(`${ env.PUBLIC_CENTRIFUGO_URL }/connection/websocket`, {});
+    const client = new Centrifuge(`${ env.PUBLIC_CENTRIFUGO_URL ?? "ws://localhost:8001" }/connection/websocket`, {});
 
     if (autoConnect) client.connect();
 
